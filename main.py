500b08a31a2699314e2e0749932aea7c18ff9738
Viliami <viliamiuchiha@gmail.com>
2015-11-16 16:01:02 +1300

    Updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, glob, time
+import os, glob, time, numpy
 from SimpleCV import *
 
 my_images_path = "/home/ubuntu/Documents/Recognition-101/images/"
@@ -13,10 +13,28 @@ imgs = list()
 directory = os.path.join(path, extension)
 files = glob.glob(directory)
 
+def gen_rect_points(blob):
+    return [(blob.minX(),blob.minY()),(blob.minX(),blob.maxY()),(blob.maxX(),blob.maxY()),(blob.maxX(),blob.minY())]
+
 for file in files:
     new_img = Image(file)
-    blobs = new_img.findCorners()
+    new_img = new_img.binarize()
+    blobs = new_img.findBlobs()
+    #edges = new_img.findCorners()
+    print("Height",blobs.height())
+    print("MaxX = ",blobs[0].maxX())
+    print("MaxY = ",blobs[0].maxY())
     if(blobs):
         blobs.draw()
+    #if(bottom_edge):
+    #    bottom_edge.draw()
+    #if(edges):
+    #    edges.draw()
+    points = gen_rect_points(blobs[0])
+    new_img.dl().polygon(points,filled = True, color=Color.RED)
     new_img.show()
+    new_img.save("images/processed.jpg")
+    #print("Area:",blobs.area())
+    #print("Angles:",blobs.angle())
+    print("Centers:",blobs.coordinates())
     time.sleep(2)
